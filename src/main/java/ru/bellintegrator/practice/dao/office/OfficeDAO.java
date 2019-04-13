cdae1a3d1311f2e966c52321e669973848c630f8
Kristina Skopintceva <christinasavina2008@yandex.ru>
2019-04-13 18:38:59 +0300

    Исправила схему таблицы

---
+++ src/main/java/ru/bellintegrator/practice/dao/office/OfficeDAO.java
@@ -0,0 +1,4 @@
+package ru.bellintegrator.practice.dao.office;
+
+public class OfficeDAO {
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,64 +2,67 @@ CREATE TABLE IF NOT EXISTS organization(
 id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 version      INTEGER NOT NULL,
 name         VARCHAR(50) NOT NULL,
-fullName     VARCHAR(100) NOT NULL,
+full_name    VARCHAR(100) NOT NULL,
 inn          VARCHAR(9) NOT NULL,
 kpp          VARCHAR(12) NOT NULL,
 address      VARCHAR(150) NOT NULL,
 phone        VARCHAR(20) NOT NULL,
-isActive     BOOLEAN NOT NULL);
+is_active    BOOLEAN NOT NULL);
 
-COMMENT ON TABLE Person IS 'Организация';
+COMMENT ON TABLE organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS office (
 id           INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
 version      INTEGER NOT NULL,
-orgid        INTEGER,
+org_id       INTEGER NOT NULL ,
 name         VARCHAR(50) NOT NULL,
 address      VARCHAR(150)NOT NULL,
 phone        VARCHAR(20) NOT NULL,
-is_active    BOOLEAN NOT NULL,
-FOREIGN KEY (orgId)REFERENCES `organization` (id));
+is_active    BOOLEAN NOT NULL);
+CREATE INDEX IX_office_org_id ON office (org_id);
+ALTER TABLE office ADD FOREIGN KEY (org_id) REFERENCES organization (id);
 
-COMMENT ON TABLE Person IS 'Офис';
-CREATE INDEX IX_Org_Id ON office (orgid);
+COMMENT ON TABLE office IS 'Офис';
+
 
 CREATE TABLE IF NOT EXISTS user (
-id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-version      INTEGER NOT NULL,
-officeId     INTEGER NOT NULL ,
-firstName    VARCHAR(50) NOT NULL ,
-secondName   VARCHAR(50) NOT NULL,
-middleName   VARCHAR(50),
-position     VARCHAR(50) NOT NULL,
-phone        VARCHAR(20),
-docCode      VARCHAR(20) NOT NULL,
-docNumber    VARCHAR(30),
-docDate      DATE,
-citizenshipCode INTEGER NOT NULL,
+id            INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+version       INTEGER NOT NULL,
+office_id      INTEGER NOT NULL ,
+first_name    VARCHAR(50) NOT NULL ,
+second_name   VARCHAR(50) NOT NULL,
+middle_name   VARCHAR(50),
+position      VARCHAR(50) NOT NULL,
+phone         VARCHAR(20),
+doc_code      INTEGER NOT NULL,
+doc_number    VARCHAR(30),
+doc_date      DATE,
+citizenship_code INTEGER NOT NULL,
+is_identified BOOLEAN NOT NULL);
+CREATE INDEX IX_user_office_id ON user (office_id);
+ALTER TABLE user ADD FOREIGN KEY (office_id) REFERENCES office (id);
 
-isidentified BOOLEAN NOT NULL,
-FOREIGN KEY (officeId) REFERENCES office (id),
-FOREIGN KEY (docCode) REFERENCES docs (code),
-FOREIGN KEY (citizenshipCode) REFERENCES countries (code));
-CREATE INDEX IX_office_Id ON user(officeId);
-CREATE INDEX IX_citizenship_Id ON user (citizenshipCode);
+CREATE INDEX IX_user_doc_code ON user (doc_code);
+ALTER TABLE user ADD FOREIGN KEY (doc_code) REFERENCES docs (code);
 
-COMMENT ON TABLE Person IS 'Пользователи';
+CREATE INDEX IX_user_citizenship_code ON user (citizenship_code);
+ALTER TABLE user ADD FOREIGN KEY (citizenship_code) REFERENCES countries (code);
+
+COMMENT ON TABLE user IS 'Пользователи';
 
 CREATE TABLE IF NOT EXISTS docs (
 code         INTEGER PRIMARY KEY NOT NULL,
 version      INTEGER NOT NULL,
 name         VARCHAR(150) NOT NULL);
 
-COMMENT ON TABLE Person IS 'Документы';
+COMMENT ON TABLE docs IS 'Документы';
 
 CREATE TABLE IF NOT EXISTS countries (
 code         INTEGER PRIMARY KEY NOT NULL,
 version      INT NOT NULL,
 name         VARCHAR(50) NOT NULL);
 
-COMMENT ON TABLE Person IS 'Страны';
+COMMENT ON TABLE countries IS 'Страны';
 
 
 
