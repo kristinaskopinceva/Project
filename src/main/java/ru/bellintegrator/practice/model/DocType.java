c45e20a2fa5c189bf7337dc871e51e3c809d1e8b
Kristina Skopintceva <christinasavina2008@yandex.ru>
2019-04-30 14:59:58 +0300

    Докуметы работников вынесены в другую табл

---
+++ src/main/java/ru/bellintegrator/practice/model/DocType.java
@@ -0,0 +1,50 @@
+package ru.bellintegrator.practice.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
+
+@Entity(name = "doc_type")
+public class DocType {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+        @Version
+        private Integer version;
+        private Integer code;
+        @Column(nullable = false,length = 150)
+        private String name;
+
+        public DocType(){ }
+
+        public DocType(Integer code, String name){
+            this.code = code;
+            this. name = name;
+        }
+        public Integer getId() {
+        return id;
+    }
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+
+    }
